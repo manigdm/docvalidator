cf109b8287946f6509300d542d94fd9b8f0675d3
Manikandan <manikandan.moorthi@mantralabsglobal.com>
2020-03-04 18:42:05 +0530

    First deployment

---
+++ precache-manifest.731655113bf01e046ec6d56badafb940.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5693527ff30fda9d73db6cace2e7dbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e082368252d930613a2",
+    "url": "/static/css/2.301fcd7a.chunk.css"
+  },
+  {
+    "revision": "e730edf1cbb3eb5e83a7",
+    "url": "/static/css/main.12c8857b.chunk.css"
+  },
+  {
+    "revision": "7e082368252d930613a2",
+    "url": "/static/js/2.f01bb453.chunk.js"
+  },
+  {
+    "revision": "e730edf1cbb3eb5e83a7",
+    "url": "/static/js/main.33cede6f.chunk.js"
+  },
+  {
+    "revision": "1a8e0bda9bc90d3f1eb7",
+    "url": "/static/js/runtime~main.d9e14d2a.js"
+  },
+  {
+    "revision": "869878d63491c7e3502cd067eabdd196",
+    "url": "/static/media/404.869878d6.png"
+  },
+  {
+    "revision": "a357d33ca0268421605017efd4a20b43",
+    "url": "/static/media/HELR45W.a357d33c.ttf"
+  },
+  {
+    "revision": "eb5d0e13b9eddb73e3fb4d257a9dbcc7",
+    "url": "/static/media/addhar-card@3x.eb5d0e13.png"
+  },
+  {
+    "revision": "432b5266570de2832201781868d50b7c",
+    "url": "/static/media/lady-vector.432b5266.png"
+  },
+  {
+    "revision": "b02307998c65843359a07d487ebfcb1b",
+    "url": "/static/media/loader.b0230799.gif"
+  },
+  {
+    "revision": "3c816b36056b019e55f5582373e1f367",
+    "url": "/static/media/login.3c816b36.png"
+  }
+]);
